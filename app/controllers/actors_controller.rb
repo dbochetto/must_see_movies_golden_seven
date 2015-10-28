a9573b3459468f4b71cfc9f7a891a91349a6acf3
DB <davidbochetto@David-Bochettos-MacBook-Pro.local>
2015-10-28 12:31:54 -0500

    DB V2 (Final v1)

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,47 @@
 class ActorsController < ApplicationController
 
-def index
-	@list_of_actors = Actor.all
-end
+	def index
+		@list_of_actors = Actor.all
+	end
+
+	def new_form
+	end
+
+	def new_actor
+		a = Actor.new
+		a.image_url = params[:the_image]
+		a.name = params[:the_name]
+		a.bio = params[:the_bio]
+		a.dob = params[:the_dob]
+		a.save
+
+		redirect_to("http://localhost:3000/actors")
+	end
+
+	def show
+		@actor = Actor.find_by({ :id => params[:id]})
+	end
+
+	def edit_form
+		@actor = Actor.find_by({ :id => params[:id]})
+	end
+
+	def update_actor
+		a = Actor.find_by({ :id => params[:id]})
+		a.name = params[:the_name]
+		a.bio = params[:the_bio]
+		a.dob = params[:the_dob]
+		a.save
+
+		redirect_to("http://localhost:3000/actors")
+	end
+
+	def destroy
+		a = Actor.find_by({ :id => params[:id]})
+		a.destroy
+
+		redirect_to("http://localhost:3000/actors")
+	end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,17 @@ Rails.application.routes.draw do
  #Routes to delete directors:
  get("/directors/delete_director/:id", { :controller => "directors", :action => "destroy"})
 
+#Routes for Actors:
+#Routes to create new actors
+get("/actors/new", { :controller => "actors", :action => "new_form"})
+get("/actors/create_actor", { :controller => "actors", :action => "new_actor"})
 #Routes to read actors:
  get("/actors", { :controller => "actors", :action => "index"})
+ get("/actors/:id", { :controller => "actors", :action => "show"})
+ #Routes to edit actors:
+ get("/actors/edit/:id", { :controller => "actors", :action => "edit_form"})
+ get("/actors/update_actor/:id", { :controller => "actors", :action => "update_actor"})
+ #Routes to delete actors:
+ get("/actors/delete_actor/:id", { :controller => "actors", :action => "destroy"})
 
 end
